a5a1f742a048475ef0e3884748c249310667bd09
Michele Invernizzi <inve.michele@gmail.com>
2022-03-04 10:19:24 +0100

    adding info ala4

--- ala4/queue_gromacs.i.sh
+++ ala4/queue_gromacs.i.sh
@@ -11,43 +11,21 @@ max_t=4:00 #h:min
 host=$HOSTNAME
 
 # Commands
-nsteps="-nsteps 10000000"
-#nsteps=""
+nsteps="-nsteps $[500*1000*20]"
 exe="`which gmx_mpi` mdrun"
 Filename=alanine
 Inputname=input
-#NumWalkers=`ls ${Inputname}*.tpr |wc -l`
-NumWalkers=1
-ncore=$[ncore*NumWalkers]
-WalkerPrefix=".$rep"
-#optWalkers="-multi $NumWalkers"
-optWalkers=""
-#if [ $NumWalkers -eq 1 ]
-#then
-#  WalkerPrefix=""
-#  optWalkers=""
-#fi
-Project=${Filename}${WalkerPrefix}
-Run_file=../../inputs/${Inputname}${WalkerPrefix}.tpr
-outfile=${Filename}${WalkerPrefix}.out
+Project=${Filename}
+Run_file=../../inputs/${Inputname}.$rep.tpr
+outfile=${Filename}.out
 max_h=`python <<< "print('%g'%(${max_t%:*}+${max_t#*:}/60-0.05))"`
 
 # Prepare Submission
-../../../bck.meup.sh -i $outfile
-res=""
-#cpt_files=`ls ${Project}*.cpt |wc -l`
-#if [ $cpt_files -gt 0 ]
-#then
-#  res="-cpi ${Project}.cpt -append"
-#  bck.meup.sh -i ${Project}*.gro > $outfile
-#else
-#  bck.meup.sh -i ${Project}* > $outfile
-#fi
-../../../bck.meup.sh -i ${Project}* > $outfile
-#sed "s/__REP__/$rep/g" plumed.dat > plumed.$rep.dat
+bck.meup.sh -i $outfile
+bck.meup.sh -i ${Project}* > $outfile
 
-mpi_cmd="$exe -plumed ../plumed.dat -maxh $max_h -s $Run_file -deffnm $Project $optWalkers $nsteps -ntomp 1 $res"
-extra_cmd="../../get_ala4_state.sh"
+mpi_cmd="$exe -plumed ../plumed.dat -maxh $max_h -s $Run_file -deffnm $Project $optWalkers $nsteps -ntomp 1"
+extra_cmd="../../get_ala4_basin.sh"
 
 ### if euler ###
 if [ ${host:0:3} == "eu-" ]
